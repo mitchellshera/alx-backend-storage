62a578ef70d5b3afe96a52c794ca0de686ec4921
mitchellshera <mitchellwachera@gmail.com>
2023-12-14 15:10:37 +0300

    5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- Create or replace the trigger
+CREATE TRIGGER reset_valid_email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
