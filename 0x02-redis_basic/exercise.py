dc646065d338799fbe3d8d6754f1f2ef850f7248
mitchellshera <mitchellwachera@gmail.com>
2023-12-28 13:02:00 +0300

    replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,75 +3,93 @@
 
 
 import redis
-import uuid
-from typing import Union, Callable
+from uuid import uuid4
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    '''count how many times methods of Cache class are called'''
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''wrap the decorated function and return the wrapper'''
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    '''store the history of inputs and outputs for a particular function'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''wrap the decorated function and return the wrapper'''
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable):
+    '''display the history of calls of a particular function.'''
+    r = redis.Redis()
+    func_name = fn.__qualname__
+    c = r.get(func_name)
+    try:
+        c = int(c.decode("utf-8"))
+    except Exception:
+        c = 0
+    print("{} was called {} times:".format(func_name, c))
+    inputs = r.lrange("{}:inputs".format(func_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(func_name), 0, -1)
+    for inp, outp in zip(inputs, outputs):
+        try:
+            inp = inp.decode("utf-8")
+        except Exception:
+            inp = ""
+        try:
+            outp = outp.decode("utf-8")
+        except Exception:
+            outp = ""
+        print("{}(*{}) -> {}".format(func_name, inp, outp))
 
 
 class Cache:
-    """
-    Cache class for storing data in Redis.
-    """
-
+    '''declares a Cache redis class'''
     def __init__(self):
-        """
-        Initialize the Cache instance with a
-        edis client and flush the database.
-        """
-        self._redis = redis.Redis()
+        '''upon init to store an instance and flush'''
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store the input data in Redis with a
-        random key and return the key.
+        '''takes a data argument and returns a string'''
+        rkey = str(uuid4())
+        self._redis.set(rkey, data)
+        return rkey
 
-        :param data: The data to be stored in the cache.
-        :type data: Union[str, bytes, int, float]
-        :return: The randomly generated key
-        used to store the data in Redis.
-        :rtype: str
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''convert the data back to the desired format'''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
 
-    def get(self, key: str, fn: Callable = None) -> Union[
-         str, bytes, int, None]:
-        """
-        Retrieve the data associated with the given key from Redis.
+    def get_str(self, key: str) -> str:
+        '''parametrize Cache.get with correct conversion function'''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
 
-        :param key: The key associated with the data.
-        :type key: str
-        :param fn: Optional callable to convert
-        the data to the desired format.
-        :type fn: Callable, optional
-        :return: The data associated with the key,
-        optionally converted by the provided callable.
-        :rtype: Union[str, bytes, int, None]
-        """
-        data = self._redis.get(key)
-        if data is not None and fn is not None:
-            return fn(data)
-        return data
-
-    def get_str(self, key: str) -> Union[str, None]:
-        """
-        Retrieve a string value associated with the given key from Redis.
-
-        :param key: The key associated with the data.
-        :type key: str
-        :return: The string value associated with the key.
-        :rtype: Union[str, None]
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        """
-        Retrieve an integer value associated with the given key from Redis.
-
-        :param key: The key associated with the data.
-        :type key: str
-        :return: The integer value associated with the key.
-        :rtype: Union[int, None]
-        """
-        return self.get(key, fn=int)
+    def get_int(self, key: str) -> int:
+        '''parametrize Cache.get with correct conversion function'''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
